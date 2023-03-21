3d361bff14fb62e134388177a25e1d267681b7f8
MxMhx <kantinan2016@gmail.com>
2023-03-21 16:10:38 +0700

    feat: Login Page and Registor Page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,49 +1,59 @@
 import React from "react";
+import Header from "../components/Header";
+import CategoryBar from "../components/CategoryBar";
 
 export default function Login() {
   return (
-    <div className="flex justify-center h-[750px] px-6 my-5 items-center">
-      <div className="w-4/6 lg:w-1/3 h-max bg-white p-5 rounded-lg">
-        <h3 className="pt-4 text-2xl text-center">Login!</h3>
-        <form>
-          <div>
-            <label className="block mb-2 text-red">Email</label>
-            <input
-              className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              type="email"
-              placeholder="Ex. test@email.com"
-            />
-          </div>
-          <div>
-            <label className="block mb-2 text-red">Password</label>
-            <input
-              className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              type="password"
-              placeholder="**********"
-            />
-          </div>
-          <div>
-            <button
-              className="w-full px-4 py-2 font-bold text-white bg-red rounded-full hover:bg-third-color focus:outline-none focus:shadow-outline"
-              type="submit"
+    <div className="font-kanit">
+      <Header />
+      <CategoryBar />
+      <div className="flex w-full bg-gray justify-center items-center py-5">
+        <div className="border-l-2 h-6 mx-2"></div>
+        <h1 className="text-xl w-1/5">Login</h1>
+      </div>
+      <div className="flex justify-center mt-5">
+        <div className="w-1/3 h-max p-5 rounded-lg">
+          <h3 className="pt-4 text-2xl text-center">Login!</h3>
+          <form>
+            <div>
+              <label className="block mb-2 text-red">Email</label>
+              <input
+                className="px-2 py-1 rounded-lg shadow-md"
+                type="email"
+                placeholder="Ex. test@email.com"
+              />
+            </div>
+            <div>
+              <label className="block mb-2 text-red">Password</label>
+              <input
+                className="px-2 py-1 rounded-lg shadow-md"
+                type="password"
+                placeholder="**********"
+              />
+            </div>
+            <div>
+              <button
+                className="w-full px-4 py-2 mt-3 font-bold text-white bg-red rounded-full"
+                type="submit"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+          <div className="my-5 flex flex-col sm:flex-row justify-between">
+            <a
+              className="text-red hover:text-third-color text-sm float-left"
+              href="/"
             >
-              Login
-            </button>
+              Forgot Password?
+            </a>
+            <a
+              className="text-red hover:text-third-color text-sm float-right"
+              href="/registor"
+            >
+              Create Account
+            </a>
           </div>
-        </form>
-        <div className="my-5 flex flex-col sm:flex-row justify-between">
-          <a
-            className="text-red hover:text-third-color text-sm float-left"
-            href="#"
-          >
-            Forgot Password?
-          </a>
-          <a
-            className="text-red hover:text-third-color text-sm float-right"
-            href="#"
-          >
-            Create Account
-          </a>
         </div>
       </div>
     </div>
